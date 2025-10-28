93f6b4888a62942d83f9a3fc25f210963813e80a
ntsanq <ntsanq@gmail.com>
2025-10-28 09:19:34 +0700

    chore: config again the typescript

---
+++ tests/model/register.model.ts
@@ -0,0 +1,14 @@
+export interface RegisterRequest {
+  first_name: string;
+  last_name: string;
+  dob: string;
+  street:  string;
+  postal_code:  string;
+  city:  string;
+  state: string;
+  country: string;
+  phone: string;
+  email: string;
+  password: string;
+}
+
