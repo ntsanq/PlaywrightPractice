6439cd5b04a4cf63be9604a9e3b3425c8ea7bba2
ntsanq <ntsanq@gmail.com>
2025-11-07 10:26:52 +0700

    chore: change the scope of the userFixtures

--- src/fixtures/user.fixture.ts
+++ src/fixtures/user.fixture.ts
@@ -1,10 +1,20 @@
-import { test } from '@playwright/test';
+import {
+  PlaywrightWorkerArgs,
+  PlaywrightWorkerOptions,
+  test,
+} from '@playwright/test';
 import fs from 'fs';
 import { UserModel } from '@/models';
 
-export const userFixtures = test.extend<{
-  loggedUser: UserModel;
-}>({
-  loggedUser: async ({}, use) =>
-    await use(JSON.parse(fs.readFileSync('.auth/auth.meta.json', 'utf-8'))),
+export const userFixtures = test.extend<
+  {}, // test-scoped fixtures
+  {
+    loggedUser: UserModel;
+  } // worker-scoped fixtures
+>({
+  loggedUser: [
+    async ({}: PlaywrightWorkerArgs & PlaywrightWorkerOptions, use) =>
+      await use(JSON.parse(fs.readFileSync('.auth/auth.meta.json', 'utf-8'))),
+    { scope: 'worker' },
+  ],
 });
